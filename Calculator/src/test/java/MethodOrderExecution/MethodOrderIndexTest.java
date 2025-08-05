e7a02124cf983e3bef7d20690d086b6c910e8723
Vighnesh Tathare <v42069@yahoo.com>
2025-08-06 00:26:24 +0530

    unit testing

--- Calculator/src/test/java/MethodOrderExecution/MethodOrderIndexTest.java
+++ Calculator/src/test/java/MethodOrderExecution/MethodOrderIndexTest.java
@@ -1,32 +1,41 @@
 package MethodOrderExecution;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class MethodOrderIndexTest {
+
+    @AfterAll
+            public void printBuilder(){
+        System.out.println(b);
+    }
+
+    StringBuilder b=new StringBuilder();
 
     @Test
     public void testA(){
+        b.append(1);
         System.out.println("TestA");
     }
 
     @Test
     public void testB(){
+        b.append(2);
         System.out.println("TestB");
     }
     @Test
     @Order(2)
     public void testC(){
+        b.append(3);
         System.out.println("TestC");
     }
 
     @Test
     @Order(1)
     public void testD(){
+        b.append(4);
         System.out.println("TestD");
     }
 }
